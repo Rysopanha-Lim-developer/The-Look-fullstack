45f8e6c54200d42d326456e023906e002ea7f9d9
Rysopanha <sopanhalimry@gmail.com>
2026-08-25 13:47:04 +0700

    initialize repository

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  //Run command below to install node module
+  //npm install babel-plugin-react-compiler@latest
+  //after install write the one line below
+  reactCompiler: true, //This line eliminate the need of useMemo React hook
+
+
+  cacheComponents: true //This enable caching system 'use cache' for cache boundaries 
 };
 
 export default nextConfig;
