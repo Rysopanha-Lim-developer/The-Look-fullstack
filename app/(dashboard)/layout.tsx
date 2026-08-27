23549c6c9b0c603b139430e674dd88e30452ae45
Rysopanha <sopanhalimry@gmail.com>
2026-08-27 14:18:50 +0700

    add nav bar and footer

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import Link from "next/link";
+import Image from "next/image"
 
 
 export default function DashboardLayout({children}: {children: React.ReactNode}){
@@ -11,9 +10,27 @@ export default function DashboardLayout({children}: {children: React.ReactNode})
             <main>
                 {children}
             </main>
-            <footer>
-
-            </footer>
+            <footer className="Footer">
+                <ul className="media">
+                    <li>Follow Us</li>
+                    <li ><Image src="/assets/Logo/Facebook.svg"  alt="Facebook" width={24} height={24} /> The Look Cambodia</li>
+                    <li ><Image src="/assets/Logo/TikTok.svg" alt="TikTok" width={24} height={24} /> @theLookCambodia</li>
+                    <li ><Image src="/assets/Logo/Instagram.svg"  alt="Instagram" width={24} height={24} /> @theLookCambodia</li>
+                    <li ><Image src="/assets/Logo/X.svg" alt="X" width={24} height={24} /> @theLookCambodia</li>
+                </ul>
+                <ul className="media">
+                    <li>Customer services</li>
+                    <li ><Image src="/assets/Logo/PrivacyPolicy.svg"  alt="Privacy Policy" width={24} height={24} /> Privacy Policy</li>
+                    <li ><Image src="/assets/Logo/Swapping.svg"  alt="Swapping" width={24} height={24} /> Item Exchange</li>
+                    <li ><Image src="/assets/Logo/MoneyLogo.svg"  alt="Money Logo" width={24} height={24} /> Cash Refund</li>
+                </ul>
+                <ul className="media">
+                    <li>Contact Us</li>
+                    <li ><Image src="/assets/Logo/Email.svg"  alt="Email" width={24} height={24} /> tLook@gmail.com</li>
+                    <li ><Image src="/assets/Logo/Telephone.svg" alt="Telephone" width={24} height={24} /> (+855) 23 888 999</li>
+                    <li ><Image src="/assets/Logo/Telegram.svg"  alt="Telegram" width={24} height={24} /> @theLookCambodia</li>
+                </ul>
+        </footer>
         </>
     )
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,11 @@
 import InfiniteLogoScroll from "../components/InfiniteLogoScroll/InfiniteLogoScroll";
+import Image from "next/image";
 
 export default function Home() {
     return (<>
-        {/* <InfiniteLogoScroll /> */}
+        <div>
+            <Image src="/assets/Banner/BigSaleBanner.jpg" alt="Sale banner" width={1200} height={100} className="w-dvw h-auto" />
+        </div>
+        <InfiniteLogoScroll />
     </>);
 }
