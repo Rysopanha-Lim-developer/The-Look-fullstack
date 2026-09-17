92f937d0b7c577629c4f7e0a70539846752dd8ce
Rysopanha <sopanhalimry@gmail.com>
2026-09-17 15:48:26 +0700

    add stronger password requirement for new account creation

--- Backend/services/CreateNewUser.ts
+++ Backend/services/CreateNewUser.ts
@@ -6,7 +6,13 @@ import z from "zod";
 export const registerSchema = z.object({
     username: z.string().min(5, "Username must be at least 5 characters").max(12, "Username must be at most 12 characters"),
     email: z.email(),
-    password: z.string().min(4, "Password must be at least 4 characters").max(8, "Password must be at most 8 characters"),
+    password: z.string()
+        .min(8, "Password must be at least 8 characters")
+        .max(10, "Password must be at most 10 characters")
+        .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+        .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+        .regex(/[0-9]/, "Password must contain at least one number")
+        .regex(/[^A-Za-z0-9]/, "Password must contain at least one special character"),
 });
 
 
@@ -16,9 +22,11 @@ export async function CreateNewUser(request:NextRequest) {
 
         const checkedInput = registerSchema.safeParse(body)
         if (!checkedInput.success) {
+            const { fieldErrors } = z.flattenError(checkedInput.error);
+            const allMessages = Object.values(fieldErrors).flat(); //convert the obj above to array for display
             return NextResponse.json(
-                { message: "Please check your input" },
-                {status: 400}
+                { message: "Please check your input", errors: allMessages },
+                { status: 400 }
             );
         }
         const {username, email, password} = checkedInput.data;
@@ -58,7 +66,7 @@ export async function CreateNewUser(request:NextRequest) {
     }catch (error: any) {
         return NextResponse.json(
         { 
-            error: error.message || "Something went wrong"
+            message: error.message || "Something went wrong"
         },
         {
             status: 500
